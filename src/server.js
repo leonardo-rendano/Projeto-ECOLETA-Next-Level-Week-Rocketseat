924ad332667edaab3a02ea0394157afdeabc10cf
Leonardo-Rendano <62267208+Leonardo-Rendano@users.noreply.github.com>
2020-06-04 19:12:51 -0300

    Configuração das rotas

--- src/server.js
+++ src/server.js
@@ -2,5 +2,21 @@
 const express = require("express")
 const server = express()
 
+// CONFIGURAR PASTA PÚBLICA
+server.use(express.static("public"))
+
+
+// CONFIGURAR CAMINHOS DA MINHA APLICAÇÃO
+// PÁGINA INICIAL
+// REQ: REQUISIÇÃO
+// RES: RESPOSTA
+server.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html")
+})
+
+server.get("/create-point", (req, res) => {
+  res.sendFile(__dirname + "/views/create-point.html")
+})
+
 // LIGAR O SERVIDOR
 server.listen(3000)
